01bc52432cf9ddb682db3d2bf0ed57d783c1f348
aaryan <aaryanbajaj385@gmail.com>
2024-09-11 10:31:19 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h2>Hello world</h2>
+      <Button>Aaryan</Button>
+    </>
+  )
+}
+
+export default App
