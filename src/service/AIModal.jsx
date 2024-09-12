b8ec035c758d613a4ad614301142a62430fe755f
aaryan <aaryanbajaj385@gmail.com>
2024-09-12 12:47:25 +0530

    corrected error in env calling

--- src/service/AIModal.jsx
+++ src/service/AIModal.jsx
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
   
-  const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_AI_API_KEYY;
+  const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_AI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
