1efbe2905bccb43ce74adafbe1f41174c6af0e82
aaryan <aaryanbajaj385@gmail.com>
2024-09-12 22:08:41 +0530

    saving trip in firebase

---
+++ src/service/firebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1DAh_quIFPgaeV1cj1dNoxq8n5SCjFI8",
+  authDomain: "tripplan-ai.firebaseapp.com",
+  projectId: "tripplan-ai",
+  storageBucket: "tripplan-ai.appspot.com",
+  messagingSenderId: "685292887560",
+  appId: "1:685292887560:web:1adc07a3efc867e05c3bbd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
